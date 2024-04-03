d9fc8f5b11b2e804710cf6dc7d516169232a05d9
yunsu-code <kws04394@gmail.com>
2024-04-03 13:29:15 +0900

    불필요한 주석 코드 삭제

--- src/pages/form/BaseListForm.jsx
+++ src/pages/form/BaseListForm.jsx
@@ -72,10 +72,7 @@ const BaseListForm = () => {
             <div key={key}>
               <div>{key}</div>
               {Object.entries(basicList[key]).map(([keys, values], idxs) => {
-                // console.log(basicList[key].length);
                 console.log(idx === 0 ? idx + idxs : idx * 3 + idxs);
-                // console.log(idxs);
-
                 return (
                   <>
                     {basicList[key][keys].type === "basic" ? (
@@ -115,135 +112,9 @@ const BaseListForm = () => {
                   </>
                 );
               })}
-              {/* {value.category === "section01" ? (
-                <>
-                  {value.type === "basic" ? (
-                    <BasicInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : value.type === "bible" ? (
-                    <BibleInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : value.type === "select" ? (
-                    <SelectBtnInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : null}
-                </>
-              ) : null} */}
             </div>
           );
         })}
-
-        {/* <h2>section01</h2> */}
-        {/* {Object.entries(basicList).map(([key, value], idx) => {
-          return (
-            <div key={idx}>
-              {value.category === "section01" ? (
-                <>
-                  {value.type === "basic" ? (
-                    <BasicInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : value.type === "bible" ? (
-                    <BibleInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : value.type === "select" ? (
-                    <SelectBtnInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : null}
-                </>
-              ) : null}
-            </div>
-          );
-        })}
-        <h2>section02</h2>
-        {Object.entries(basicList).map(([key, value], idx) => {
-          return (
-            <div key={idx}>
-              {value.category === "section02" ? (
-                <>
-                  {value.type === "basic" ? (
-                    <BasicInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : value.type === "bible" ? (
-                    <BibleInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : value.type === "select" ? (
-                    <SelectBtnInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : null}
-                </>
-              ) : null}
-            </div>
-          );
-        })}
-        <h2>section03</h2>
-        {Object.entries(basicList).map(([key, value], idx) => {
-          return (
-            <div key={idx}>
-              {value.category === "section03" ? (
-                <>
-                  {value.type === "basic" ? (
-                    <BasicInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : value.type === "bible" ? (
-                    <BibleInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : value.type === "select" ? (
-                    <SelectBtnInputType
-                      label={value.name}
-                      target={key}
-                      ref={(el) => (inputRef.current[idx] = el)}
-                      idx={idx}
-                    />
-                  ) : null}
-                </>
-              ) : null}
-            </div>
-          );
-        })} */}
         <div>
           <AddTextListType label={"공지사항"} ref={noticeRef} />
         </div>
