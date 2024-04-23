ad9c5c505529a9b39c35bfecf1d72fe1069d77fb
yunsu-code <kws04394@gmail.com>
2024-04-23 13:38:43 +0900

    패키지 삭제로 인한 코드 삭제

--- src/pages/view/Main.jsx
+++ src/pages/view/Main.jsx
@@ -1,13 +1,11 @@
 import Container from "@components/view/Container";
 import { Link } from "react-router-dom";
 import styles from "./Main.module.scss";
-import { Reset } from "styled-reset";
 import Header from "@components/common/Header";
 
 const Main = () => {
   return (
     <>
-      <Reset />
       <Header />
       <Container type="form">
         <div className={styles.mainWrap}>
